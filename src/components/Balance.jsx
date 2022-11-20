b68735f38b83fbb686f4587373a9a88bc70b73dd
SageKyle <ominyipaulo@gmail.com>
2022-11-20 11:38:33 +0100

    added a balance component

---
+++ src/components/Balance.jsx
@@ -0,0 +1,15 @@
+// styles/ images
+import Logo from '../assets/images/logo.svg';
+import './Balance.scss';
+
+export const Balance = ({ balance }) => {
+	return (
+		<section className="balance">
+			<div className="balance__content">
+				<h4 className="balance__content--title">My balance</h4>
+				<h2 className="balance__content--number">${balance}</h2>
+			</div>
+			<img src={Logo} alt="logo" className="balance__logo" />
+		</section>
+	);
+};
--- src/components/Charts.jsx
+++ src/components/Charts.jsx
@@ -1,9 +1,14 @@
+import { Balance } from './Balance';
+
 // styles
 import './Charts.scss';
+
+const balance = 924.18;
 
 export const Charts = () => {
 	return (
 		<div className="chart">
+			<Balance balance={balance} />
 			<h3>This is the main chart container</h3>
 		</div>
 	);
