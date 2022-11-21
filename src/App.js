516ac341167ed43889d4b3a4d56b5309bd548d7e
SageKyle <ominyipaulo@gmail.com>
2022-11-21 19:10:41 +0100

    styled the root App component
    - moved the Balance component from Chart to the App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.scss';
 
+import { Balance } from './components/Balance';
 import { Charts } from './components/Charts';
 
 function App() {
+	const balance = 924.18;
+
 	return (
 		<div className="App">
-			<h3>This is the app component</h3>
+			<Balance balance={balance} />
 			<Charts />
 		</div>
 	);
--- src/components/Charts.jsx
+++ src/components/Charts.jsx
@@ -1,14 +1,9 @@
-import { Balance } from './Balance';
-
 // styles
 import './Charts.scss';
-
-const balance = 924.18;
 
 export const Charts = () => {
 	return (
 		<div className="chart">
-			<Balance balance={balance} />
 			<h3>This is the main chart container</h3>
 		</div>
 	);
